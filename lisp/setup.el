8923c9344ec9182e3800ca4d00629d5652ae4c37
John Kitchin <jkitchin@andrew.cmu.edu>
2016-08-29 21:20:31 -0400

    add overlay function and rescale latex options.

--- lisp/setup.el
+++ lisp/setup.el
@@ -1,1 +1,10 @@
+(defun org-toggle-latex-overlays (arg)
+  "Toggle LaTeX fragments.  The prefix ARG is passed to `org-preview-latex-fragment'."
+  (interactive "P")
+  (if (org--list-latex-overlays)
+      (org-remove-latex-fragment-image-overlays)
+    (org-toggle-latex-fragment '(16)))
+  nil)
 
+
+(plist-put org-format-latex-options :scale 1.5)
