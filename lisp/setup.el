0924ef10fa298db55420db72ea39756ba78876ae
John Kitchin <jkitchin@andrew.cmu.edu>
2016-09-08 15:35:24 -0400

    add turn it in function to setup
    the update feature is not as good as I would like, so I am going to put updates here instead.

--- lisp/setup.el
+++ lisp/setup.el
@@ -7,4 +7,43 @@
   nil)
 
 
-(plist-put org-format-latex-options :scale 1.5)
+(plist-put org-format-latex-options :scale 1.8)
+
+(defun tq-turn-it-in ()
+  "Save all buffers, add files, create a SYSTEM-INFO file, commit them and push.
+Check *techela log* for error messages."
+  (interactive)
+  (tq-insert-system-info)
+
+  ;; Let's assume turning in will work, and set the time.
+  (gb-set-filetag "TURNED-IN" (current-time-string))
+
+  ;; make sure all buffers are saved
+  (save-some-buffers t t)
+  
+  (mygit "git add *")
+  
+  (let ((status (car (mygit "git commit -am \"turning in\""))))
+    (unless (or (= 0 status)		; no problem
+		(= 1 status))		; no change in files
+      (gb-set-filetag "TURNED-IN"
+		      (concat "Failed: " (current-time-string)))
+      (switch-to-buffer "*techela log*")
+      (error "Problem committing.  Check the logs")))
+
+  (unless (= 0 (car (mygit "git push -u origin master")))
+    (mygit "git commit --amend -m \"*** TURNING IN FAILED ***.\"")
+    (gb-set-filetag "TURNED-IN" (concat "Failed: " (current-time-string)))
+    (save-buffer)
+    (switch-to-buffer "*techela log*")
+    (error "Problem pushing to server.  Check the logs"))
+
+  (save-buffer)
+  (message
+   (let ((choices '("Woohoo, you turned it in!"
+		    "Awesome, you rocked that turn in!"
+		    "Way to go, you turned it in!"
+		    "Great job, you turned it in!"
+		    "Sweet, you turned it in!"
+		    "Booya, you turned it in!")))
+     (nth (cl-random (length choices)) choices))))
