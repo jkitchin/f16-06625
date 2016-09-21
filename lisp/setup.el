e7e3607f210abf6449d66067700cc8bab2d46ffe
John Kitchin <jkitchin@andrew.cmu.edu>
2016-09-21 18:49:08 -0400

    use more general pull

--- lisp/setup.el
+++ lisp/setup.el
@@ -84,9 +84,9 @@ Check *techela log* for error messages."
 	 (progn 
 	   (mygit "git add *")
 	   (mygit "git commit -am \"my changes\"") 
-	   (mygit "git pull origin master")
+	   (mygit "git pull") 
 	   (mygit "git commit -am \"commit post pull\""))
-       (mygit "git pull origin master"))
+       (mygit "git pull"))
 
      ;; now, open the file
      (find-file (expand-file-name
